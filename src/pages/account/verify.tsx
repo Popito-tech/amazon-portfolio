ef174d8b48a826c6bd2393787d1573149e654b5d
Popito-tech <wirtouce@outlook.fr>
2024-01-14 23:37:59 +0100

    Your commit message here

--- src/pages/account/verify.tsx
+++ src/pages/account/verify.tsx
@@ -9,22 +9,28 @@ const VerifyPage: React.FC = () => {
 export default VerifyPage
 
 
-export const getServerSideProps:GetServerSideProps = async ({query: {
-  token}}) => {
-    try{
-    const {user}= verifyToken(token)
-    const userExist = await User.findOne({email: user.email})
-    if(!userExist){
+export const getServerSideProps: GetServerSideProps = async ({ query: { token } }) => {
+  try {
+    console.log('Start getServerSideProps');
+
+    const { user } = verifyToken(token);
+    console.log('Verified token:', user);
+
+    const userExist = await User.findOne({ email: user.email });
+    console.log('User exists:', userExist);
+
+    if (!userExist) {
       await connectMongoDB();
       await User.create(user);
-    };
-  } catch (error){
-    console.log({error});
-}
+      console.log('User created:', user);
+    }
+
+    console.log('End getServerSideProps');
+  } catch (error) {
+    console.error('Error in getServerSideProps:', error);
+  }
 
   return {
-    props: {
-      
-    }
-  }
-}
+    props: {},
+  };
+};
